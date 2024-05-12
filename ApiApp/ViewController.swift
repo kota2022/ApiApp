2d1e800ff95924c0cd57ec393d792c9cdb5c3c0c
kota tsuji <kota.tsuji2022@klas.ch>
2024-05-12 17:10:44 +0900

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by Tsuji Kota on 12.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
